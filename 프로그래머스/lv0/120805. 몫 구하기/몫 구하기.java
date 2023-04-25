d56a8e9131fd51790eebafa406129a676e968784
Sangho Kim <72784596+sangho527@users.noreply.github.com>
2023-04-25 18:17:21 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
@@ -1,8 +1,9 @@
 class Solution {
-    public int solution(int num1, int num2) {
+    public int solution(int num1, int num2){
+        int result = 0;
         if ((0 < num1 && num1 <= 100) && (0 < num2 && num2 <= 100)){
+        result = num1 / num2;
         }
-        int result = num1 / num2;
         return result;
     }
 }
