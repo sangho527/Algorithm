8234fad7e2d2e52556e816e73e9cf40a3fd2ef14
Sangho Kim <72784596+sangho527@users.noreply.github.com>
2023-05-13 12:54:43 +0900

    [level 0] Title: 편지, Time: 0.13 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        String[] list = message.split("");
+        int listLength = list.length;
+        
+        answer = listLength*2;
+        
+        return answer;
+    }
+}
